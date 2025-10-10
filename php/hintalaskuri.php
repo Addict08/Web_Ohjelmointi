8ce710a90c2ae88b2a29dde96761d3e113fd9913
Addict08 <randommikael7@gmail.com>
2025-10-10 11:13:12 +0300

    c5
    rder

---
+++ php/hintalaskuri.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <title>Hintalaskuri</title>
+</head>
+<body>
+    <h1>Tuotteen Hintatiedot</h1>
+<?php
+$tuotteen_nimi = "Sähköpotkulauta"
+$hinta_kpl = 349.90 //liukuluku, hinta per kappale
+$kappalemaara = 2 //kokonaisluku
+$alennusprosentti = 15 //kokonaisluku
+
+$VäliSumma = $hinta_kpl * $kappalemaara;
+
+$AlennusMäärä = $VäliSumma * ($alennusprosentti / 100);
+
+$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
