2e4cd7e9df7ed02b73fe24f48b023bd6adbd25fc
Addict08 <randommikael7@gmail.com>
2025-10-10 08:48:20 +0300

    4
    4

--- php/Kirjautuminen.php
+++ php/Kirjautuminen.php
@@ -2,12 +2,13 @@
 // Tarkistetaan onko lomake lähetetty
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['admin'];
+    $Txt1 = "Tervetuloa Admin";
     $passWord = $_POST['Kissa123'];
         // Tarkistetaan käyttäjätunnus ja salasana
         if ($user === "admin" && $passWord === "Kissa123") {
             // Kirjautuminen onnistui
-            echo "<p>Tervetuloa Admin</p>";
-            echo "<p>Sinut ohjataan palkkalaskuriin muutaman sekunnin kuluttua...</p>";
+            echo "<h3>$Txt1</h3>";
+            echo "Sinut ohjataan palkkalaskuriin muutaman sekunnin kuluttua...";
             // Ohjataan käyttäjä palkkalaskuriin 2 sekunnin kuluttua
             echo '<meta http-equiv="refresh" content="2;url=palkkalaskuri.php">';
         }
